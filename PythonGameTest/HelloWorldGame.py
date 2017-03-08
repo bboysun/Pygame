a2c3b83b1e47f7d1635a0ec79b35429c51678d89
bboysun <948575906@qq.com>
2017-03-08 19:11:52 +0800

    my first python game, hello world & lettersRain

---
+++ PythonGameTest/HelloWorldGame.py
@@ -0,0 +1,28 @@
+background_image_filename = 'resources/sushiplate.jpg'
+mouse_image_filename = 'resources/fugu.png'
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+pygame.display.set_caption("Hello, world!")
+
+background = pygame.image.load(background_image_filename).convert()
+mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    screen.blit(background, (0,0))
+    x, y = pygame.mouse.get_pos()
+    x-= mouse_cursor.get_width() / 2
+    y-= mouse_cursor.get_height() / 2
+##    print("x is ", x)
+##    print("y is ", y)
+    screen.blit(mouse_cursor, (x, y))
+
+    pygame.display.update()
